07b86fe007a9e95e61aec9f5c22d7d8ce8da575e
BharatSharma29 <bharat429sharma@gmail.com>
2023-08-19 18:43:55 +0530

    second commit

--- data.js
+++ data.js
@@ -1,32 +1,37 @@
-const characterData = {
-    hero: {
-        name: "Wizard",
-        avatar: "images/wizard.png",
-        health: 60,
-        diceCount: 3,
-        currentDiceScore: []
-    },
-    orc: {
-        name: "Orc",
-        avatar: "images/orc.png",
-        health: 30,
-        diceCount: 1,
-        currentDiceScore: []
-    },
-    demon: {
-        name: "Demon",
-        avatar: "images/demon.png",
-        health: 25,
-        diceCount: 2,
-        currentDiceScore: []
-    },
-    goblin: {
-        name: "Goblin",
-        avatar: "images/goblin.png",
-        health: 20,
-        diceCount: 3,
-        currentDiceScore: []
-    }
-}
+import Wizard from "images/wizard.png";
+import Orc from "images/orc.png";
+import Demon from "images/demon.png";
+import Goblin from "images/goblin.png";
 
-export default characterData
+const characterData = {
+  hero: {
+    name: "Wizard",
+    avatar: { Wizard },
+    health: 60,
+    diceCount: 3,
+    currentDiceScore: [],
+  },
+  orc: {
+    name: "Orc",
+    avatar: { Orc },
+    health: 30,
+    diceCount: 1,
+    currentDiceScore: [],
+  },
+  demon: {
+    name: "Demon",
+    avatar: { Demon },
+    health: 25,
+    diceCount: 2,
+    currentDiceScore: [],
+  },
+  goblin: {
+    name: "Goblin",
+    avatar: { Goblin },
+    health: 20,
+    diceCount: 3,
+    currentDiceScore: [],
+  },
+};
+
+export default characterData;
