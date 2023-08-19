04864223bce47ab82f97f7126bfa438cf3f2b550
BharatSharma29 <bharat429sharma@gmail.com>
2023-08-19 19:17:26 +0530

    updated path

--- data.js
+++ data.js
@@ -1,33 +1,33 @@
-import Wizard from "images/wizard.png";
-import Orc from "images/orc.png";
-import Demon from "images/demon.png";
-import Goblin from "images/goblin.png";
+import Wizard from "./images/wizard.png";
+import Orc from "./images/orc.png";
+import Demon from "./images/demon.png";
+import Goblin from "./images/goblin.png";
 
 const characterData = {
   hero: {
     name: "Wizard",
-    avatar: { Wizard },
+    avatar: Wizard,
     health: 60,
     diceCount: 3,
     currentDiceScore: [],
   },
   orc: {
     name: "Orc",
-    avatar: { Orc },
+    avatar: Orc,
     health: 30,
     diceCount: 1,
     currentDiceScore: [],
   },
   demon: {
     name: "Demon",
-    avatar: { Demon },
+    avatar: Demon,
     health: 25,
     diceCount: 2,
     currentDiceScore: [],
   },
   goblin: {
     name: "Goblin",
-    avatar: { Goblin },
+    avatar: Goblin,
     health: 20,
     diceCount: 3,
     currentDiceScore: [],
